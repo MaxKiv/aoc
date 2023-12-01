dcd043ee878b31f9590717d0d809302202220aa2
Kivits Max (HC-CS/XCT-Dev2) <Max.Kivits@nl.bosch.com>
2023-12-01 16:47:36 +0100

    day 1 rust part 1

--- 2023/rust/day_1/src/main.rs
+++ 2023/rust/day_1/src/main.rs
@@ -1,22 +1,43 @@
 use std::io;
 
+const DIGITS: [&str; 9] = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
+
 fn main() {
     let mut answer_a : u32 = 0;
-    let mut answer_b : u32 = 0;
-
-    for line in io::stdin().lines() {
-        answer_a = answer_a + solve_a(line.unwrap());
-        println!("{}", answer_a);
-    }
 
     println!("Part A: {}", answer_a);
+
+    for line in io::stdin().lines() {
+        let cur = solve_a(line.as_ref().unwrap());
+        println!("{}: {}", line.unwrap(), cur);
+        answer_a += cur;
+    }
+
     println!("Part B: {}", answer_b);
+    let mut answer_b : u32 = 0;
 }
 
-fn solve_a(line : String) -> u32{
-    let a = line.chars().find_map(|c| c.to_digit(10)).unwrap();
-    let b = line.chars().rev().find_map(|c| c.to_digit(10)).unwrap();
-    return a + b
+fn solve_a(line : &str) -> u32{
+    line.chars().rev().find_map(|c| c.to_digit(10)).unwrap() + 10 * line.chars().find_map(|c| c.to_digit(10)).unwrap()
+}
 
-    line.chars().find_map(|c| c.to_digit(10)).unwrap() + line.chars().rev().find_map(|c| c.to_digit(10)).unwrap();
+fn solve_b(line : &str) -> u32{
+    let test : String = String::from("asdstwo1908fivesdf");
+    test.chars()
+        .enumerate()
+        .filter(|&(_, c)| c == DIGITS.iter().chars().next())
+        .peekable( // somehow get next 5 chars "tok" and check for DIGITS.iter().any(|s|
+    // s.contains(tok)
+        )
+
 }
