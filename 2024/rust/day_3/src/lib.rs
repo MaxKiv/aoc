8ec47ade3120e98475ea995147e8de0e72a927d4
MaxKiv <maxkivits42@gmail.com>
2025-12-01 15:01:41 +0100

    wip: saving old 2024

---
+++ 2024/rust/day_3/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn solve(input: &str) -> (usize, usize) {
+    // steal from
+    // https://github.com/Shad0wRim/AoC/blob/8a4b5f05e60b01171027d98241e6cc1cc0b21b20/aoc2024/src/days/day03.rs#L101
+}
--- 2024/rust/day_3/src/main.rs
+++ 2024/rust/day_3/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+use day_3::solve;
+
+fn main() {}
+
+#[test]
+fn p1_example() {
+    let input = fs::read_to_string("example").unwrap();
+    let (p1, _) = solve(&input);
+    assert_eq!(p1, 161);
 }
